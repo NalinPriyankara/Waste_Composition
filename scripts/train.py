c4544dc13f4ceaee063e8cbec464897385fe5bba
NalinPriyankara <nalinpriyankara033@gmail.com>
2025-02-21 16:46:21 +0530

    update1.0.1

--- scripts/train.py
+++ scripts/train.py
@@ -1,17 +1,17 @@
 from ultralytics import YOLO
 
-# Load a pre-trained YOLO model
-model = YOLO("yolov8n.pt")  # Load YOLOv8 Nano (smallest model)
+if __name__ == '__main__':
+    # Load a pre-trained YOLO model
+    model = YOLO("yolov8n.pt")
 
-# Train the model
-results = model.train(
-    data="E:\Project\Waste_Composition\data\waste_dataset.yaml",  # Path to dataset config file
-    epochs=50,                       # Number of training epochs
-    batch=16,                        # Batch size
-    imgsz=640,                       # Image size
-    name="waste_detection"           # Name of the training run
-)
-
-# Evaluate the model
-metrics = model.val()  # Evaluate on the validation set
-print("mAP:", metrics.box.map)  # Print mean Average Precision (mAP)
+    # Train the model
+    results = model.train(
+        data="data/waste_dataset.yaml",
+        epochs=50,
+        batch=16,
+        imgsz=640,
+        device=0,  # Use GPU 0
+        name="waste_detection",
+        workers=0,  # Set workers to 0 to avoid multiprocessing issues
+        emp=False
+    )
